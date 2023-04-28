34bf1518b19ebb9d3feba5080aa0a74762361198
Augustin <adebureaux@gmail.com>
2023-04-28 19:00:00 +0200

    create header

---
+++ src/components/header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import 'src/styles/header.css';
+const Header = () => (
+  <div className="header">
+    <div>
+        <img width={250} height={250} src="https://upload.wikimedia.org/wikipedia/commons/e/e8/Tesla_logo.png" alt="Tesla" />
+        <p>Augustin Debureaux</p>
+    </div>
+  </div>
+)
+export default Header;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Header from '../components/header';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <Header />
+      </Head>
       <body>
         <Main />
         <NextScript />
