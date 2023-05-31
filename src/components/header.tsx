0dfeba74adab1631b935d513810dc24753ad8de9
Augustin Debureaux <adebureaux@gmail.com>
2023-05-31 17:28:16 +0200

    Header wip

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,44 +1,29 @@
 "use client";
 
 import React from 'react';
+import Link from 'next/link';
 import Image from 'next/image';
-
-
-const stats = [
-  { name: 'Offices worldwide', value: '12' },
-  { name: 'Full-time colleagues', value: '300+' },
-  { name: 'Hours per week', value: '40' },
-  { name: 'Paid time off', value: 'Unlimited' },
-]
+import { Popover } from "@headlessui/react";
+import { Bars3Icon } from "@heroicons/react/24/solid"
 
 const Header = () => (
-  <dl className="mt-16 grid grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-2 lg:grid-cols-4">
-  {stats.map((stat) => (
-    <div key={stat.name} className="flex flex-col-reverse">
-      <dt className="text-base leading-7 text-gray-300">{stat.name}</dt>
-      <dd className="text-2xl font-bold leading-9 tracking-tight text-white">{stat.value}</dd>
+  <Popover>
+  <div className="container mx-auto flex items-center border-b-2 px-6 py-2 h-24">
+    <Image className="rounded-full border" src="/images/profile.png" width={75} height={75} alt="Profile Picture" />
+    <h1 className="font-bold px-5">Augustin Debureaux</h1>
+    <div className="grow">
+      <div className="flex items-center justify-end gap-2 md:gap-8">
+        <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus-outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+          <span className="sr-only">Open menu</span>
+          <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+          <Link href="home">Home</Link>
+          <Link href="blog">Blog</Link>
+          <Link href="about">About</Link>
+          <Link href="contact">Contact</Link>
+        </Popover.Button>
+      </div>
     </div>
-  ))}
-  </dl>
-  // <div className="container">
-  //     <div className="flex">
-  //         <Image className="rounded-full" width={75} height={75} src="/images/profile.png" alt="picture" />
-  //         <span className="my-auto">Augustin Debureaux</span>
-  //     </div>
-  //     <div className="container">
-  //       <div className="flex-auto">
-  //         <button className="rounded-full">Home</button>
-  //       </div>
-  //       <div className="flex-auto">
-  //         <button className="rounded-full">About</button>
-  //       </div>
-  //       <div className="flex-auto">
-  //         <button className="rounded-full">Project</button>
-  //       </div>
-  //       <div className="flex-auto">
-  //         <button className="rounded-full">Contact</button>
-  //       </div>
-  //     </div>
-  //   </div>
+  </div>
+  </Popover>
 )
 export default Header;
